759e9e85b9f1cacf0b9abf0e03f3133a6031d9fb
menghui <mzk10@qq.com>
2018-09-04 16:00:36 +0800

    update

--- src/com/mh/myservice/action/TestAction.java
+++ src/com/mh/myservice/action/TestAction.java
@@ -1,7 +1,6 @@
 package com.mh.myservice.action;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -11,8 +10,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 
 import com.mh.myservice.core.Action;
 
@@ -25,154 +24,92 @@ public class TestAction extends Action {
 
 	@Override
 	public Object deFault() throws ServletException, IOException, SQLException {
+		getResponse().sendRedirect("test.apk");
 		return null;
 	}
 
 	public Object log() throws ServletException, IOException, SQLException {
-		ServletInputStream is = getRequest().getInputStream();
-		int len = 0;
-		byte[] buffer = new byte[1024];
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		while ((len = is.read(buffer)) != -1) {
-			bos.write(buffer, 0, len);
-		}
-		byte[] byteArray = bos.toByteArray();
-		String log = new String(byteArray, "UTF-8");
-
-		/*
-		 * // String log = getParameter("log"); String[] s = log.split(","); byte[] b =
-		 * new byte[s.length]; for (int i = 0; i < s.length; i++) { try { b[i] = (byte)
-		 * Integer.parseInt(s[i]); } catch (Exception e) { } } log = new String(b,
-		 * "UTF-8");
-		 */
+		String log = getParameter("log");
 		String ip = getRequest().getRemoteAddr();
-		System.out.println("ip="+ip);
-		System.out.println("log="+log);
-		saveLog(log, "log_" + ip + ".log");
+		System.out.println("ip=" + ip);
+		System.out.println("log=" + log);
+		
+		String realPath = getServletContext().getRealPath("log");
+		System.out.println(realPath);
+		saveLog(realPath, log, "log_" + ip + ".log");
 		return null;
 	}
 
-	public Object logv2() throws ServletException, IOException, SQLException {
-		// ServletInputStream is = getRequest().getInputStream();
-		// int len = 0;
-		// byte[] buffer = new byte[1024];
-		// ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		// while ((len = is.read(buffer)) != -1) {
-		// bos.write(buffer, 0, len);
-		// }
-		// byte[] byteArray = bos.toByteArray();
-		// String log = new String(byteArray, "UTF-8");
-		// System.out.println(log);
+	public Object err() throws ServletException, IOException, SQLException {
 		String devices = getParameter("devices");
 		String msg = getParameter("log");
-		// System.out.println(devices);
-		// System.out.println(msg);
 		String ip = getRequest().getRemoteAddr();
-		saveLog(msg, devices + "_" + ip + ".log");
-		return null;
-	}
-
-	public Object checktoken() throws ServletException, IOException, SQLException {
-		String token = (String) getSession().getAttribute("token");
-		if (token != null && "lksjaflkjoihg032opi4oigasdoihvb9wa".equals(token)) {
-			sendRedirect("test?app=showlog");
-			return null;
-		}
-
-		String key = getParameter("key");
-		if (key != null && "95279527".equals(key)) {
-			sendRedirect("checktoken.html");
-		}
-		return null;
-	}
-
-	public Object dochecktoken() throws ServletException, IOException, SQLException {
-		String key = getParameter("key");
-		if (key != null && "95279527".equals(key)) {
-			getSession().setAttribute("token", "lksjaflkjoihg032opi4oigasdoihvb9wa");
-			sendRedirect("test?app=showlog");
-		} else {
-			sendRedirect("checktoken.html");
-		}
+		String realPath = getServletContext().getRealPath("err");
+		saveLog(realPath, msg, devices + "_" + ip + ".log");
 		return null;
 	}
 
 	public Object showlog() throws ServletException, IOException, SQLException {
-//		String token = (String) getSession().getAttribute("token");
-//		if (token == null || "".equals(token)) {
-//			sendRedirect("checktoken.html");
-//		} else {
-			//sendRedirect("showlog.html");
-			String file = loadFileString("showlog.html");
-//		}
-		return file;
+		RequestDispatcher dispatcher = getRequest().getRequestDispatcher("/WEB-INF/html/showlog.html");
+		dispatcher.forward(getRequest(), getResponse());
+//		String file = loadFileString("showlog.html");
+//		return file;
+		return null;
 	}
 
 	public Object getfilelist() throws ServletException, IOException, SQLException {
-//		String token = (String) getSession().getAttribute("token");
-//		if (token != null && "lksjaflkjoihg032opi4oigasdoihvb9wa".equals(token)) {
-			File file = new File("E:/log");
-			if ((file.exists() && file.isDirectory()) || file.mkdirs()) {
+		String realPath = getServletContext().getRealPath("log");
+		File file = new File(realPath);
+		if ((file.exists() && file.isDirectory()) || file.mkdirs()) {
+		}
+		File[] listFiles = file.listFiles();
+		if (listFiles != null) {
+			List<String> list = new ArrayList<>();
+			for (File file2 : listFiles) {
+				list.add(file2.getName());
 			}
-			File[] listFiles = file.listFiles();
-			if (listFiles != null) {
-				List<String> list = new ArrayList<>();
-				for (File file2 : listFiles) {
-					// Log log = new Log();
-					// log.setLog(file2.getPath());
-					// log.setName(file2.getName());
-					// String content = loadFileString(file2.getPath());
-					// log.setLog(content);
-					list.add(file2.getName());
-				}
-				return createResponseData(200, list);
-			}
-//		}
+			return createResponseData(200, list);
+		}
 		return createResponseData(203, null);
 	}
 
 	public Object getlogfile() throws ServletException, IOException, SQLException {
-//		String token = (String) getSession().getAttribute("token");
-//		if (token != null && "lksjaflkjoihg032opi4oigasdoihvb9wa".equals(token)) {
-			String name = getParameter("filename");
-			File file = new File("E:/log");
-			if ((file.exists() && file.isDirectory()) || file.mkdirs()) {
-			}
-			File[] listFiles = file.listFiles();
-			if (listFiles != null) {
-				for (File file2 : listFiles) {
-					String filename = file2.getName();
-					if (filename.equals(name)) {
-						
-						StringBuilder sb = new StringBuilder();
-						
-						FileInputStream fis = new FileInputStream(file2);
-						InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
-						BufferedReader br = new BufferedReader(isr);
-						
-						while(br.ready()) {
-							sb.append(br.readLine()+"</br>");
-						}
-						br.close();
-						isr.close();
-						fis.close();
-						return createResponseData(200, sb.toString());
+		String name = getParameter("filename");
+		String realPath = getServletContext().getRealPath("log");
+		File file = new File(realPath);
+		if ((file.exists() && file.isDirectory()) || file.mkdirs()) {
+		}
+		File[] listFiles = file.listFiles();
+		if (listFiles != null) {
+			for (File file2 : listFiles) {
+				String filename = file2.getName();
+				if (filename.equals(name)) {
+					StringBuilder sb = new StringBuilder();
+					FileInputStream fis = new FileInputStream(file2);
+					InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+					BufferedReader br = new BufferedReader(isr);
+					while (br.ready()) {
+						sb.append(br.readLine() + "</br>");
 					}
+					br.close();
+					isr.close();
+					fis.close();
+					return createResponseData(200, sb.toString());
 				}
 			}
-//		}
+		}
 		return createResponseData(203, null);
 	}
 
-	private synchronized static void saveLog(final String msg, final String name) {
+	private synchronized static void saveLog(final String dir, final String msg, final String name) {
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				File dir = new File("E:/log/");
-				if (!dir.exists()) {
-					dir.mkdirs();
+				File file_dir = new File(dir);
+				if (!file_dir.exists()) {
+					file_dir.mkdirs();
 				}
-				File file = new File(dir, name);
+				File file = new File(file_dir, name);
 				FileOutputStream fos = null;
 				try {
 					fos = new FileOutputStream(file, true);
